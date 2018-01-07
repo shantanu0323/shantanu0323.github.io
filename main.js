6e0dc26e8001f8a79fa9d41ec1100f4659bd601a
shantanu0323 <srs.shaan@gmail.com>
2018-01-07 17:09:04 +0530

    feat: Resizing added
    * Landscape mode added for mobile

--- main.js
+++ main.js
@@ -1,10 +1,23 @@
 $(window).load(function () {
     setTimeout(function () {
-//                $("#preloader-bg").css("display", "none");
-//                $(".main-container").css("display", "block");
+        //                $("#preloader-bg").css("display", "none");
+        //                $(".main-container").css("display", "block");
         $("#preloader-bg").fadeOut(2000);
         $(".main-container").fadeIn(2000);
     }, 2000);
+
+    if (getCookie("isResizing") == "true") {
+        $("#preloader-text").removeClass("glitch-load");
+        $("#preloader-text").addClass("glitch-resize");
+        $("#preloader-text").text("RESIZING");
+        $("#preloader-text").css("letter-spacing", "5vw");
+        setCookie("isResizing", "false", 1);
+    } else {
+        $("#preloader-text").removeClass("glitch-resize");
+        $("#preloader-text").addClass("glitch-load");
+        $("#preloader-text").text("LOADING");
+        $("#preloader-text").css("letter-spacing", "7vw");
+    }
 
     var logo = $("#logo");
     var me = $("#me");
@@ -73,7 +86,7 @@ $(window).load(function () {
                             letSpaceVal = "1vw";
                             padLeftVal = "0vw";
                         }
-                        
+
                         if (screen.width >= 960) {
                             fSizeVal = "6vw";
                         }
@@ -93,3 +106,32 @@ $(window).load(function () {
     }, 4000);
 
 });
+
+window.onresize = function (e) {
+    $("#preloader-bg").fadeIn(400);
+    $(".main-container").fadeIn(400);
+    setCookie("isResizing", "true", 1);
+    location.reload();
+};
+
+function setCookie(cname, cvalue, exdays) {
+    var d = new Date();
+    d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
+    var expires = "expires=" + d.toUTCString();
+    document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+}
+
+function getCookie(cname) {
+    var name = cname + "=";
+    var ca = document.cookie.split(';');
+    for (var i = 0; i < ca.length; i++) {
+        var c = ca[i];
+        while (c.charAt(0) == ' ') {
+            c = c.substring(1);
+        }
+        if (c.indexOf(name) == 0) {
+            return c.substring(name.length, c.length);
+        }
+    }
+    return "";
+}

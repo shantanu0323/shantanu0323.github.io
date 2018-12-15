1d7a0d9008a04c212f0287edd2be741cc6c346e2
Shantanu Pramanik <srs.shaan@gmail.com>
2018-12-15 22:08:10 +0530

    Navigation Active Scrolling Setup

--- beta/assets/libraries/home.js
+++ beta/assets/libraries/home.js
@@ -19,7 +19,42 @@ $(window).scroll(function () {
 });
 
 $(".nav-link").click(function () {
-    var $anchor = $(this);
+    var anchor = $(this);
+    setActive(anchor);
+});
+
+var setActive = function (anchor) {
     $(".navbar .active").removeClass("active");
-    $anchor.parent().addClass("active");
+    anchor.parent().addClass("active");
+};
+
+$(document).scroll(function () {
+    var scrollPos = $(document).scrollTop();
+    var aboutPos = $("#about").position().top;
+    var servicesPos = $("#services").position().top;
+    var projectsPos = $("#projects").position().top;
+    var achievementsPos = $("#achievements").position().top;
+    var contactPos = $("#contact").position().top;
+    var navHome = $(".nav-item a[href='#home']");
+    var navAbout = $("a[href='#about']");
+    var navServices = $("a[href='#services']");
+    var navProjects = $("a[href='#projects']");
+    var navAchievements = $("a[href='#achievements']");
+    var navContact = $("a[href='#contact']");
+    var halfScr = aboutPos / 2;
+
+    if (scrollPos < (aboutPos - halfScr)) {
+        setActive(navHome);
+    } else if (scrollPos >= aboutPos && scrollPos < (servicesPos  - halfScr)) {
+        setActive(navAbout);
+    } else if (scrollPos >= servicesPos && scrollPos < (projectsPos  - halfScr)) {
+        setActive(navServices);
+    } else if (scrollPos >= projectsPos && scrollPos < (achievementsPos  - halfScr)) {
+        setActive(navProjects);
+    } else if (scrollPos >= achievementsPos && scrollPos < (contactPos  - halfScr)) {
+        setActive(navAchievements);
+    } else if (scrollPos >= contactPos) {
+        setActive(navContact);
+    }
+
 });
